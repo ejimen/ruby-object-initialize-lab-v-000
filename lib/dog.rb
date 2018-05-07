1c226a023b9577ce8dce0feefbe5d5ef2f0a05a4
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-07 04:18:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@ class Dog
 
 def initialize(name, breed)
   @name = name
-  @breed = "Mutt"
+  @breed = breed
 end
 
 def name=(name, breed)
   @name = name
-  @breed = "Mutt"
+  @breed = breed
 end
 
 def name
